a90a5c9ddeb777fe2ef12ca70f18acb48001c7a5
rrahadis <rahadi.solehudin@gmail.com>
2020-07-12 14:56:01 +0700

    learn PWA

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "firstpwa-v1";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/about.html",
+  "/pages/skill.html",
+  "/pages/contact.html",
+  "/pages/project.html",
+  "/css/materialize.min.css",
+  "/css/style.css",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+  "/assets/image-profile.png",
+  "/assets/twitter.png",
+  "/assets/facebook.png",
+  "/assets/instagram.png",
+  "/assets/whatsapp.png",
+  "/assets/mail.png",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+ 
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function(response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+ 
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
